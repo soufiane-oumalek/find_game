325d576f762bf0e8952043f3bcfbca1d909092f1
soufiane-oumalek <soufian.oumalek@gmail.com>
2023-12-06 20:31:13 +0100

    mastering backend

---
+++ app/route.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+from functions.games import get_games, get_game
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+@app.route("/")
+def home():
+  games = get_games()
+  print(games)
+  return "{}".format(games)
+
+@app.route("/<onegame>")
+def game(onegame):
+  game = get_game(onegame)
+  return game
+
+
+
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', port=5000, debug=False)
