83848828e46792865ccf5a1cd0f67fc5937df157
omarIdhmaid <o.idhmaid@gmail.com>
2023-12-07 18:27:25 +0100

    separate genres

--- functions/games.py
+++ functions/games.py
@@ -1,17 +1,25 @@
 import requests
 
+
 def get_games():
-  first_list = []
-  url = "https://www.freetogame.com/api/games?"
-  games_list = requests.get(url).json()
-  for i in range(0, 20):
-    first_list.append(games_list[i])
-  return first_list
+    genres = {}
+    url = "https://www.freetogame.com/api/games?"
+    games_list = requests.get(url).json()
+    for i in range(0, 100):
+        if games_list[i]["genre"] not in genres:
+            genres[games_list[i]["genre"]] = []
+        genres[games_list[i]["genre"]].append(games_list[i])
+
+    for genre in list(genres.keys()):
+        if len(genres[genre]) < 4:
+            del genres[genre]
+    return genres
+
 
 def get_game(onegame):
-  url = "https://www.freetogame.com/api/games?"
-  games_list = requests.get(url).json()
-  for game in games_list:
-    if onegame == game["title"]:
-      return game
-  return {"error": "this game doesn't exist"}
+    url = "https://www.freetogame.com/api/games?"
+    games_list = requests.get(url).json()
+    for game in games_list:
+        if onegame == game["title"]:
+            return game
+    return {"error": "this game doesn't exist"}
